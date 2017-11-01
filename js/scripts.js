58c48b127b87ded863c6517fe74df03d4faf8a57
rwart <32432397+rwart@users.noreply.github.com>
2017-11-01 18:57:04 +0100

    ready for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 
+function Button(text) {
+  this.text = text || 'Hello';
+}
+
+Button.prototype = {
+  create: function () {
+    var _this = this;
+    this.$element = $('<button>');
+    this.$element.text(this.text);
+    this.$element.click(function () {
+      alert(_this.text);
+    });
+
+    this.$element.appendTo($('body'));
+  },
+};
+var btn1 = new Button('Hello!');
+btn1.create();
